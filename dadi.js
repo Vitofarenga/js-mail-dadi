52aaaa7154bbdb9d35ee71808d20aacb0c141f5e
Vito Farenga <vitofarenga@libero.it>
2022-06-17 17:25:50 +0200

    generazione numero IA funzionante

--- dadi.js
+++ dadi.js
@@ -1,7 +1,10 @@
-const userChoice = prompt ("Scegliere un numero da 1 a 6")
+const userChoice = parseInt(prompt ("Scegliere un numero da 1 a 6"));
 if((userChoice <= 6) && (userChoice != 0)){
-    console.log("Il tuo numero è:" + userChoice)
+    console.log("Il tuo numero è: " + userChoice);
 }
 else{
-    console.log("Il numero inserito non è valido")
+    alert("Il numero inserito non è valido");
 }
+
+const iaChoice = Math.floor( (Math.random() * 6) + 1);
+console.log("Il numero dell'IA è: " + iaChoice );
