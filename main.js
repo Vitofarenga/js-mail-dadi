885d0c96a4d70bb484036350cd5191d1cec82224
Vito Farenga <vitofarenga@libero.it>
2022-06-17 16:40:38 +0200

    esercizio mail finito e funzionante

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const authorized = ['vito@libero.it', 'simone@libero.it', 'diego@libero.it', 'fede@libero.it']
-let userMail = prompt("inserisci la tua mail");
 
-for ( let index = 0; index < authorized.length; index++){
+let userInsert = prompt("inserisci la tua mail");
+
+let userMail = false
+
+for ( let index = 0; index < authorized.length ; index++){
     const element = authorized[index]
-    if(userMail == element){
-        alert('Accesso autorizzato')
-    }
-    else if(userMail != element){
-        alert('Accesso negato')
+    if(userInsert == element){
+        userMail = true;
     }
 }
+console.log(userMail);
